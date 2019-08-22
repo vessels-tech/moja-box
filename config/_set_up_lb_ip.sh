2d0f0213a665f750569877549673e4c3dcb0fb93
Lewis Daly <lewisdaly@me.com>
2019-08-22 20:17:29 +0800

    add sed workaround for macos mojave

--- config/_set_up_lb_ip.sh
+++ config/_set_up_lb_ip.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 TF_VAR_lb_public_ip=`make print-lb-ip`
-sed -i "s/TF_VAR_lb_public_ip=.*$/TF_VAR_lb_public_ip=${TF_VAR_lb_public_ip}/g" ${DIR}/gcp.private.sh
+sed -i '' "s/TF_VAR_lb_public_ip=.*$/TF_VAR_lb_public_ip=${TF_VAR_lb_public_ip}/g" ${DIR}/gcp.private.sh
 #CLUSTER_IP is an old holdover to the manual method of setting the CLUSTER_IP
-sed -i "s/CLUSTER_IP=.*$/CLUSTER_IP=${TF_VAR_lb_public_ip}/g" ${DIR}/gcp.private.sh
+sed -i '' "s/CLUSTER_IP=.*$/CLUSTER_IP=${TF_VAR_lb_public_ip}/g" ${DIR}/gcp.private.sh
