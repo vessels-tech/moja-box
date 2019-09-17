e658604aff66ca055bd27d01577e9554f54e1a48
Lewis Daly <lewisdaly@me.com>
2019-09-17 15:07:27 +0300

    start to configure newman to set up the deployment

---
+++ mojaloop_config/_test_newman.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/../config/.compiled_env
+source $DIR/../config/colors.sh
+
+## A script to test out using newman along with the Mojaloop Postman collection to set up a deployment
+
+#set this if you'd like, otherwise maybe default to another folder in this repo?
+PATH_TO_POSTMAN_REPO="/Users/lewisdaly/developer/vessels/mojaloop/postman"
+COLLECTION="OSS-New-Deployment-FSP-Setup.postman_collection.json"
+ENVIRONMENT="environments/Mojaloop-Local.postman_environment.json"
+
+#TODO: configure the enviroment somehow
+
+# Check that newman exists
+newman -v > /dev/null 2>&1 || logErr 'newman is not installed. Install newman with `npm install -g newman` and try again'
+
+# Run the collection
+
+newman run ${PATH_TO_POSTMAN_REPO}/${COLLECTION} \
+  -e ${PATH_TO_POSTMAN_REPO}/${ENVIRONMENT} \
+  --delay-request 10 \
+  --bail
